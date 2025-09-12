d4c7e372624a01df2afe0694caee298ebfd598e6
George Vasiliadis <gvasiliadis72@gmail.com>
2025-09-12 16:58:50 +1000

    feat\!: added regions, commented out first regions, explored arrays and functions.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,90 @@
 console.log("Hello World from the index.js file.");
+
+// // #region Basic conditional logic and for loops
+
+// let customPortNumber = null;
+
+// // const PORT = customPortNumber || 5000;
+// const PORT = customPortNumber ??= 5000;
+
+// // console.log("Port is " + PORT);
+// console.log("Custom port number is " + customPortNumber);
+
+// let dinner = "butter chicken";
+
+// // Loose equality, dinner will be coerced into a string.
+// if (dinner == "butter chicken") {
+//     console.log("I'm full")
+// } else {
+//     console.log("I'm ready for dessert")
+// }
+
+// let luckyNumber = 42;
+
+// // Strict equality, comparison will not be true.
+// if (luckyNumber === "42") {
+//     console.log("Lucky number is the answer")
+// } else {
+//     console.log("Lucky number is nice, sure, but ehh...")
+// }
+
+// // if (dinner == "butter chicken") console.log("I'm full")
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);
+// }
+
+// let fruits = ["apple", "mango", "banana"]
+
+// // FOR loops are best for performance
+// for (let index = 0; index < fruits.length; index++) {
+//     const fruit = fruits[index];
+//     console.log(fruit);
+// }
+
+// // FOREACH is better for readability for devs, but it's a wrapper around FOR, so it's slower than FOR
+// fruits.forEach(fruit => {
+//     console.log(fruit);
+// });
+
+// //#endregion
+
+//#region Functions
+
+function examplefunction(){
+    console.log("Function code happens here");
+}
+
+examplefunction();
+
+function messageRepeater(messageToRepeat){
+    console.log(messageToRepeat + messageToRepeat + messageToRepeat);
+    console.log("Arrow function here.  Here is the value of this:" + this);
+}
+
+for (let index = 0; index < 2; index++) {
+    messageRepeater("Hello ");
+}
+
+let favouriteDinners = [
+    "palak paneer",
+    "katsu curry",
+    "dumplings",
+    "lagham",
+    "lentil bolognese"
+]
+
+// favouriteDinners.forEach(dinner => {
+//     messageRepeater(dinner);
+// })
+
+favouriteDinners.forEach(dinner => messageRepeater(dinner));
+
+const messageRepeaterArrowFunction = (messageToRepeat) => {
+    console.log("Arrow function here.  Here is the value of this:" + this);
+}
+messageRepeaterArrowFunction("bananas")
+
+
+
+//#endregion
